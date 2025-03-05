f399df518f96675e8b69e681dbd13a00f0fee218
you416 <1604215846@qq.com>
2025-03-06 00:36:54 +0800

    Update 一个简单的PyQt窗口.py

--- Chapter1 PyQt基础知识/一个简单的PyQt窗口.py
+++ Chapter1 PyQt基础知识/一个简单的PyQt窗口.py
@@ -1,10 +1,8 @@
-# 创建一个PyQt窗口
-
 import sys
 from PyQt5.QtWidgets import QApplication, QLabel
 
 if __name__ == "__main__":
-    app = QApplication([])
+    app = QApplication([]) #实例化一个QApplication对象用于接收一个列表的值，如果需要传入命令行参数可以传入sys.argv
     label = QLabel('Hello, PyQt!')
     label.show()
     sys.exit(app.exec_())
