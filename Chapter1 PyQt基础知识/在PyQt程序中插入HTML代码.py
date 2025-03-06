f88d952f85473047085983d9027108276092564a
you416 <1604215846@qq.com>
2025-03-06 11:20:40 +0800

    Update 在PyQt程序中插入HTML代码.py

--- Chapter1 PyQt基础知识/在PyQt程序中插入HTML代码.py
+++ Chapter1 PyQt基础知识/在PyQt程序中插入HTML代码.py
@@ -1,1 +1,10 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel
 
+if __name__ == "__main__":
+    app = QApplication([])
+    label = QLabel()
+    with open('test.html', 'r', encoding="utf-8") as f:
+        label.setText(f.read())
+    label.show()
+    sys.exit(app.exec_())
